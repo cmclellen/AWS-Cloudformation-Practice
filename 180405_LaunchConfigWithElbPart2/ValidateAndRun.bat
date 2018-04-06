24038e5dfe2b96c94d751b78497223a9889f0fd7
Craig McLellen <craig.mclellen@rhipe.com>
2018-04-06 13:50:03 +1000

    qw

--- 180405_LaunchConfigWithElbPart2/ValidateAndRun.bat
+++ 180405_LaunchConfigWithElbPart2/ValidateAndRun.bat
@@ -3,4 +3,4 @@ setlocal
 
 aws cloudformation validate-template --template-body file://launchconfig-with-elb-pt2.cf.json
 
-aws cloudformation create-stack --stack-name craig12341222 --template-body file://launchconfig-with-elb-pt2.cf.json
+aws cloudformation create-stack --stack-name craig123412221221 --template-body file://launchconfig-with-elb-pt2.cf.json 
