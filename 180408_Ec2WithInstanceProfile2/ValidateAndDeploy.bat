80ff88c092b5ac5324d67c09b7a985a9d9ee5e8a
Craig <cmclellen@gmail.com>
2018-04-08 12:36:06 +1000

    werwe

---
+++ 180408_Ec2WithInstanceProfile2/ValidateAndDeploy.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+
+set STACK_NAME=craig444
+
+call aws cloudformation validate-template --template-body file://ec2-with-instanceprofile.cf.json
+if %errorlevel% neq 0 goto error
+
+call aws cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name %STACK_NAME% --template-body file://ec2-with-instanceprofile.cf.json
+if %errorlevel% neq 0 goto error
+
+echo.
+echo !! Successfully created stack
+goto eof
+
+:error
+echo.
+echo !! An error occurred
+goto eof
+
+:eof
+echo.
